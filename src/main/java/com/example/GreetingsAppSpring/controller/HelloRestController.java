17072f74a32d49e527dd7a75c188ff9688b5f2f7
smanaspratap <manaspratap2003@gmail.com>
2026-03-24 20:16:05 +0530

    [Manas]: UC3_HelloWorld_REST_Full Completed

--- src/main/java/com/example/GreetingsAppSpring/controller/HelloRestController.java
+++ src/main/java/com/example/GreetingsAppSpring/controller/HelloRestController.java
@@ -1,13 +1,40 @@
 package com.example.GreetingsAppSpring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.example.GreetingsAppSpring.model.User;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloRestController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Hello from Manas";
+    // GET - http://localhost:8080/hello
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello from BridgeLabz!!!";
+    }
+
+    // GET - http://localhost:8080/hello/query?name=Manas
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public String sayHelloQuery(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+    }
+
+    // GET - http://localhost:8080/hello/param/Manas
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    // POST - http://localhost:8080/hello/post
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+    }
+
+    // PUT - http://localhost:8080/hello/put/Manas?lastName=Singh
+    @PutMapping("/put/{firstName}")
+    public String sayHelloPut(@PathVariable String firstName,
+                              @RequestParam(value = "lastName") String lastName) {
+        return "Hello " + firstName + " " + lastName + "!";
     }
 }
