844b5aaeeb12619b939be2a311e79f8382613de0
smanaspratap <manaspratap2003@gmail.com>
2026-03-24 20:21:11 +0530

    [Manas]: UC5_SpringConcepts_Logger Completed

--- src/main/java/com/example/GreetingsAppSpring/GreetingsAppSpringApplication.java
+++ src/main/java/com/example/GreetingsAppSpring/GreetingsAppSpringApplication.java
@@ -1,6 +1,8 @@
 package com.example.GreetingsAppSpring;
 
 import com.example.GreetingsAppSpring.component.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,9 +10,11 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class GreetingsAppSpringApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(GreetingsAppSpringApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
+		logger.debug("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(GreetingsAppSpringApplication.class, args);
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
 	}
 }
