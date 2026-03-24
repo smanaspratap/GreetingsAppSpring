824bd5ec32e3a7513b0d8d4c3bf84ecc284c231f
smanaspratap <manaspratap2003@gmail.com>
2026-03-24 20:05:20 +0530

    [Manas]: UC1_HelloWorld_REST Completed

---
+++ src/main/java/com/example/GreetingsAppSpring/GreetingsAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.GreetingsAppSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsAppSpringApplication.class, args);
+	}
+
+}
